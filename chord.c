b1ff88190390145614b2bf66c811b972fefeb3c3
Zachary Michaelov <zach.michaelov@gmail.com>
2014-03-04 02:33:12 -0500

    basic communication working

--- chord.c
+++ chord.c
@@ -6,39 +6,63 @@
 
 const char* my_address = "127.0.0.1";
 
-int handle_connection(int connfd) {
-    char request[MAXBUF];
+char * read_request(int connfd) {
+    char* request = Malloc(sizeof(char)*MAXBUF);
     rio_t client;
     Rio_readinitb(&client, connfd);
-
     Rio_readlineb(&client, request, MAXBUF);
+    return request;
+}
+
+
+int handle_connection(int connfd) {
+
+    char* request = read_request(connfd);
     printf("%s\n", request);
+    char* cmd = strtok(request, "|");
+    printf("command: %s\n", cmd);
+	if(!strcmp(cmd, "JOIN")) {
+        char* ip = strtok(NULL, ":");
+        int port = atoi(strtok(NULL, "\r\n"));
+        printf("address and port: %s:%d\n", ip, port);
+
+    }
+    // free dynamic memory
+    Free(request);
     return 0;
 }
+// join an existing chord ring
+void join_chord_ring(char* ip, int port){
+    int joinfd = Open_clientfd(ip, port);
+    // send bytes request to join
+    char request[MAXBUF];
+    size_t n = sprintf(request, "JOIN|%s:%d\r\n", ip, port);
+    Rio_writep(joinfd, request, n);
+
+    // response should be pointers to successor node in ring
+}
 int main(int argc, char *argv[]) {
-    int join_ip, join_port, connfd;
+    int join_port, connfd;
     struct sockaddr_in clientaddr;
     struct hostent *hp;
-    char *haddrp;
+    char *haddrp, *join_ip;
     char ip_and_port[21];
     unsigned char myhash[SHA_DIGEST_LENGTH];
     // parse command line options
     if (argc < 2 || argc > 4) {
         printf("Usage: %s listen_port [join_ip] [join_port]\n", argv[0]);
         exit(1);
-    } //else if (argc == 2) {
-
-   // }
-    else if (argc == 4) {
-        join_ip = atoi(argv[2]);
+    } else if (argc == 4) {
+        join_ip = argv[2];
         join_port = atoi(argv[3]);
+        join_chord_ring(join_ip, join_port);
     }
     // initialize listening
     int listen_port = atoi(argv[1]); // listen for chord connections on this port
     size_t n = sprintf(ip_and_port, "%s:%d", my_address, listen_port);
-    printf("%s\n", ip_and_port);
+    //printf("%s\n", ip_and_port);
     SHA1(ip_and_port, n, myhash);
-    printf("My hash position is: %02x\n", myhash);
+    printf("My hash position is: %x\n", myhash);
 
     int listenfd = Open_listenfd(listen_port);
     int optval = 1;
@@ -58,7 +82,7 @@ int main(int argc, char *argv[]) {
 
         haddrp = inet_ntoa(clientaddr.sin_addr);
         printf("Accepted connection from %s:%d\n", haddrp, clientaddr.sin_port);
-        handle_connection(connfd);
+        handle_connection(connfd);// TODO use detached threads
 
     }
     //char data[] = "JOIN|127.0.0.1:8085";
